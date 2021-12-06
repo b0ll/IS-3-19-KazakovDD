c097353e04d774a657b8ae5fd95b4aeeed667361
b0ll <pelmeshkademo@mail.ru>
2021-12-06 18:53:47 +0500

    Добавьте файлы проекта.

---
+++ IS-3-19-KazakovDD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_3_19_KazakovDD
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
