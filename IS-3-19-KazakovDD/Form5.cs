c18f0fa5b19b9992a8d3acd488e0e4d30da37257
b0ll <pelmeshkademo@mail.ru>
2021-12-06 22:25:17 +0500

    Задание 5

--- IS-3-19-KazakovDD/Form5.cs
+++ IS-3-19-KazakovDD/Form5.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MyLibrary;
 
 namespace IS_3_19_KazakovDD
 {
@@ -15,6 +17,49 @@ namespace IS_3_19_KazakovDD
         public Form5()
         {
             InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnDB conndb = new ConnDB();
+            if (textBox1.TextLength != 0)
+            {
+                try
+                {
+                    conndb.ConnMySql().Open();
+                    string commandStr = $"INSERT INTO t_PraktStud (fioStud, datetimeStud) VALUES (@fio,@date)";
+                    using (MySqlCommand command = new MySqlCommand(commandStr, conndb.ConnMySql()))
+                    {
+                        command.Parameters.Add("@fio", MySqlDbType.VarChar).Value = textBox1.Text;
+                        command.Parameters.Add("@date", MySqlDbType.DateTime).Value = dateTimePicker1.Text;
+                        command.Connection.Open();
+                    
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex}");
+                }
+                finally
+                {
+                    MessageBox.Show("Соединение закрыто.");
+                    conndb.ConnMySql().Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните ФИО");
+            }
         }
     }
 }
